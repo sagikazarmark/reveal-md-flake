d3b216f0b1a3ee7872210b4674451607267cff4e
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2021-10-16 01:43:48 +0200

    build: update nix
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- flake.nix
+++ flake.nix
@@ -4,15 +4,14 @@
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     reveal-md-src = {
-      url = "github:webpro/reveal-md?rev=06b9605bd9cf7bb2186b263df141d76d50aa09ac"; # 5.1.3
+      url = "github:webpro/reveal-md/5.1.3";
       flake = false;
     };
   };
 
   outputs = { self, nixpkgs, flake-utils, reveal-md-src }:
     flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
+      let pkgs = import nixpkgs { inherit system; };
       in rec {
         packages.reveal-md = pkgs.mkYarnPackage {
           name = "reveal-md";
@@ -20,8 +19,11 @@
           packageJSON = ./package.json;
           yarnLock = ./yarn.lock;
         };
+
         defaultPackage = packages.reveal-md;
 
-        devShell = pkgs.mkShell { buildInputs = [packages.reveal-md]; };
+        overlay = final: prev: { reveal-md = prev.defaultPackage; };
+
+        devShell = pkgs.mkShell { buildInputs = [ packages.reveal-md ]; };
       });
 }
